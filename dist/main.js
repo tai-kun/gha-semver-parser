5ef6093c228763c34f570487ddd8b84f3b0b5622
tai-kun <taikun.dev@gmail.com>
2026-01-05 09:49:27 +0900

    最初のコミット

---
+++ dist/main.js
@@ -0,0 +1,33 @@
+import * as core from "@actions/core";
+// @ts-expect-error
+import semverParse from "semver/functions/parse";
+try {
+    const safeParse = core.getBooleanInput("safe-parse");
+    const version = core.getInput("version", { required: true });
+    const v = semverParse(version);
+    if (v) {
+        core.setOutput("version", v.version);
+        core.setOutput("major", v.major);
+        core.setOutput("minor", v.minor);
+        core.setOutput("patch", v.patch);
+        core.setOutput("prerelease", v.prerelease.join("."));
+        core.setOutput("build", v.build.join("."));
+    }
+    else if (safeParse) {
+        core.setOutput("version", "");
+    }
+    else {
+        throw new Error(`Failed to parse semver: ${JSON.stringify(version)}`);
+    }
+}
+catch (ex) {
+    let err;
+    if (ex instanceof Error) {
+        err = ex;
+    }
+    else {
+        err = new Error("Failed to parse semver", { cause: ex });
+    }
+    core.error(err);
+    core.setFailed(err);
+}
---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import * as core from "@actions/core";
+import type { SemVer } from "semver";
+// @ts-expect-error
+import semverParse from "semver/functions/parse";
+
+try {
+  const safeParse = core.getBooleanInput("safe-parse");
+  const version = core.getInput("version", { required: true });
+  const v: SemVer = semverParse(version);
+  if (v) {
+    core.setOutput("version", v.version);
+    core.setOutput("major", v.major);
+    core.setOutput("minor", v.minor);
+    core.setOutput("patch", v.patch);
+    core.setOutput("prerelease", v.prerelease.join("."));
+    core.setOutput("build", v.build.join("."));
+  } else if (safeParse) {
+    core.setOutput("version", "");
+  } else {
+    throw new Error(`Failed to parse semver: ${JSON.stringify(version)}`);
+  }
+} catch (ex) {
+  let err: Error;
+  if (ex instanceof Error) {
+    err = ex;
+  } else {
+    err = new Error("Failed to parse semver", { cause: ex });
+  }
+
+  core.error(err);
+  core.setFailed(err);
+}
